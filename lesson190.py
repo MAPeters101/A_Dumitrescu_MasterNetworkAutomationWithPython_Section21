934a4d1d20ded96197c4e0daeeb99c8649de2bc1
Mark Peters <mapeters101@earthlink.net>
2024-11-16 12:14:00 -0500

    Lesson190:  Work on lesson.

---
+++ lesson190.py
@@ -0,0 +1,21 @@
+
+movie = 'The Godfather'
+director = 'Francis Ford Coppola'
+movie_and_director = movie + director
+print(movie_and_director)
+print(movie + ' was directed by ' + director)
+
+print('abc' '123')
+language = 'Python '
+version = 3.11
+#print(language + version)
+print(language + str(version))
+print()
+
+print(movie * 5)
+print('#' * 50)
+price = '10.5'
+print(price * 5)
+print(float(price) * 5)
+
+
