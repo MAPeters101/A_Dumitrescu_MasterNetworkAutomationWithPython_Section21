03e7b41ec41750d512697e8ef12843f5157c4405
Mark Peters <mapeters101@earthlink.net>
2024-11-15 23:34:29 -0500

    Lesson186:  Work on lesson.

---
+++ lesson186.py
@@ -0,0 +1,29 @@
+my_str1 = 'I learn Python.'
+my_str2 = "I learn Python."
+
+print(my_str1)
+
+# Hi there! I'm Andrei.
+#print('Hi there! I'm Andrei.')
+print('Hi there! I\'m Andrei.')
+print("Hi there! I'm Andrei.")
+print()
+
+message = 'He said: "Go for it!"'
+print(type(message))
+
+languages = """I like Python
+Golang
+and
+Solidity.
+"""
+print(languages)
+
+my_languages = 'I like Python\nGolang\nand\nSolidity.'
+print(my_languages)
+
+print('a\tb\tc\td\ne')
+print('\\n')
+
+print('He says: "I\'m 20"')
+print('\\ is a special character in Python.')
