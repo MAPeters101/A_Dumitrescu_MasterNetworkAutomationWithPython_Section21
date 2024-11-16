e72e82b6b65970877a825f5107d36fdcd9d73a20
Mark Peters <mapeters101@earthlink.net>
2024-11-16 12:06:58 -0500

    Lesson189:  Work on lesson.

---
+++ lesson189.py
@@ -0,0 +1,16 @@
+movie = 'The Godfather'
+print(movie[0])
+print(movie[1])
+print(movie[4])
+print(movie[-1])
+print(movie[-3])
+
+#print('Python'[7])
+print(len('Python'))
+s1 = 'I love Python programming!'
+n = len(s1)
+print(s1[n-1])
+
+print(s1[0])
+#s1[0] = 'X'
+print(s1)
