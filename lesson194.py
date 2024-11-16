6c2f62a0c516ac6949d403365af3eb3e36749fa3
Mark Peters <mapeters101@earthlink.net>
2024-11-16 13:07:07 -0500

    Lesson194:  Complete work on lesson from 6:50.

--- lesson194.py
+++ lesson194.py
@@ -36,9 +36,50 @@ txt = 'I learn PytHon, pythoN is cool!'
 n = txt.lower().count('python')
 print(n)
 
+print(txt.count('y'))
+print()
 
+my_list = txt.split()
+print(my_list)
 
+txt = 'I le arn PytH    on, pythoN is c\nool!'
+n = txt.lower().count('python')
+print(n)
 
+my_list = txt.split()
+print(my_list)
+print()
+
+print('10.1.2.3'.split('.'))
+print()
+
+ip = '10.1.2.3'
+ip_list = ip.split('.')
+print(ip_list)
+
+ip_str = '.'.join(ip_list)
+print(ip_str)
+
+ip_str = ','.join(ip_list)
+print(ip_str)
+
+ip_str = '-'.join(ip_list)
+print(ip_str)
+
+ip_str = 'xxxxxx'.join(ip_list)
+print(ip_str)
+print()
+
+my_str = 'I learn Python Programming.'
+print(my_str.find('Python'))
+print(my_str.find('Pyxthon'))
+print('Python' in my_str)
+print('Golang' in my_str)
+print()
+
+print('Python' not in my_str)
+print('Golang' not in my_str)
+print()
 
 
 
