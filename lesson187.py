0a793aa43806da7892aca881a623af14f635c80d
Mark Peters <mapeters101@earthlink.net>
2024-11-15 23:41:12 -0500

    Lesson187:  Work on lesson.

---
+++ lesson187.py
@@ -0,0 +1,19 @@
+
+# name = input()
+# print('Your name is ', name)
+
+# name = input('Enter your name: ')
+# print('Your name is ', name)
+# print(type(name))
+
+# price = input('Enter price: ')
+# quantity = input('Enter quantity: ')
+# total_value = price * quantity
+# print(total_value)
+
+price = input('Enter price: ')
+quantity = input('Enter quantity: ')
+total_value = float(price) * float(quantity)
+print(total_value)
+
+
