0dcd15dbe9512d9ff3e0daa034f5d73c5c8c7339
Mark Peters <mapeters101@earthlink.net>
2024-11-16 12:20:01 -0500

    Lesson191:  Work on lesson.

---
+++ lesson191.py
@@ -0,0 +1,19 @@
+
+movie = 'The Godfather'
+print(movie[0])
+print(movie[0:2])
+print(movie[2:5])
+print(movie[:2])
+print(movie[4:])
+print(movie[-2:])
+print(movie[:4] + movie[4:])
+print()
+
+print(movie[:42])
+print(movie[3:100])
+print(movie[0:10:2])
+print(movie[::])
+print(movie[::-1])
+
+
+
