5714059ae0a81dfc3be241f8f963dc59bb40b024
Mark Peters <mapeters101@earthlink.net>
2024-11-16 12:40:49 -0500

    Lesson193:  Work on lesson.

---
+++ lesson193.py
@@ -0,0 +1,17 @@
+# Functions: print(), len(), type(), sum(), max(), min(), round()
+print(len('xfdsa'))
+
+# Methods
+print(dir(str))
+help(str.replace)
+
+s = 'Python'
+new_s = s.upper()
+print(s)
+print(new_s)
+
+print('prOgramING'.lower())
+
+
+
+
