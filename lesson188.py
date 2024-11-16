5e3228569c410a506404d525c11246d27118c566
Mark Peters <mapeters101@earthlink.net>
2024-11-15 23:55:10 -0500

    Lesson188:  Complete work on lesson from 3:50.

--- lesson188.py
+++ lesson188.py
@@ -1,8 +1,40 @@
 
 # 1 mile = 1.609 km
-miles = float(input('Enter distance in miles: '))
-print(type(miles))
-# miles = float(miles)
-km = miles * 1.609
-print('Distance in km:', km)
+# miles = float(input('Enter distance in miles: '))
+# print(type(miles))
+# # miles = float(miles)
+# km = miles * 1.609
+# print('Distance in km:', km)
+print()
+
+a = 10
+b = 2.5
+c = '8.2'
+
+a_float = float(a)
+print('a:', type(a))
+print('a_float:', type(a_float))
+print()
+
+b_int = int(b)
+print('b_int:', type(b_int))
+print()
+
+b_str = str(b)
+print('b_str:', type(b_str))
+print()
+
+c_float = float(c)
+print('c_float:', type(c_float))
+print()
+
+#c_int = int(c)
+c_int = int(float(c))
+print('c_int', type(c))
+
+# str = 's'
+# a = 1
+# a_str = str(a)
+
+
 
